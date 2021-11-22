03f8795fd2a89ce91bdca223c8bb75be67397abb
Dalk-GitHub <67603460+Dalk-GitHub@users.noreply.github.com>
2021-11-22 16:33:01 +0100

    Create TestSerializer.cs

---
+++ Tests/TestSerializer.cs
@@ -0,0 +1,14 @@
+﻿using TestType = System.Int32;
+using System;
+using Dalk.PropertiesSerializer.TypeSerializers;
+
+[TypeSerializer]
+public class TestTypeSerializer : ITypeSerializer
+{
+    public object Deserialize(string o) => TestType.Parse(o);
+
+    Type type = typeof(TestType);
+    public Type GetCType() => type;
+
+    public string Serialize(object o) => o.ToString();
+}
