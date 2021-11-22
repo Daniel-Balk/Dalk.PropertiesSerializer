86a547cbd6f563dd32a86f82709bf5d7c4bdb474
Dalk-GitHub <67603460+Dalk-GitHub@users.noreply.github.com>
2021-11-22 17:49:41 +0100

    Create GenProperty.cs

---
+++ properties2csharp/GenProperty.cs
@@ -0,0 +1,9 @@
+﻿namespace properties2csharp
+{
+    internal class GenProperty
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string PropertyName { get; set; }
+    }
+}
