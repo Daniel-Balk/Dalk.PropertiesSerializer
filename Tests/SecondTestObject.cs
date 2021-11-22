334d986c18b56eaa5917d6f2ed01fdb21a256ef6
Dalk-GitHub <67603460+Dalk-GitHub@users.noreply.github.com>
2021-11-22 16:32:54 +0100

    Update SecondTestObject.cs

--- Tests/SecondTestObject.cs
+++ Tests/SecondTestObject.cs
@@ -1,16 +1,9 @@
 ﻿using System;
 
-namespace Tests
+public class SecondTestObject
 {
-    public class SecondTestObject
-    {
-        public SecondTestObject()
-        {
-
-        }
-        public int TestInt { get; set; } = 101;
-        public string TestString { get; set; } = "aaa";
-        public bool TestBool { get; set; } = true;
-        public DateTime Date { get; set; } = DateTime.Now;
-    }
+    public int TestInt { get; set; } = 101;
+    public string TestString { get; set; } = "aaa";
+    public bool TestBool { get; set; } = true;
+    public DateTime Date { get; set; } = DateTime.Now;
 }
