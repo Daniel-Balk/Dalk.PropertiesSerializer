93947496cb62336ae08c20589f152d2dfcdae21c
Dalk-GitHub <67603460+Dalk-GitHub@users.noreply.github.com>
2021-11-22 15:47:58 +0100

    Create SecondTestObject.cs

---
+++ Tests/SecondTestObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tests
+{
+    public class SecondTestObject
+    {
+        public SecondTestObject()
+        {
+
+        }
+        public int TestInt { get; set; } = 101;
+        public string TestString { get; set; } = "aaa";
+        public bool TestBool { get; set; } = true;
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
