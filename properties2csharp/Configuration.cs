6bfab7aa1654dc69c3cf51fc69855dbcab28875d
Dalk-GitHub <67603460+Dalk-GitHub@users.noreply.github.com>
2021-11-22 19:21:39 +0100

    Update Configuration.cs

--- properties2csharp/Configuration.cs
+++ properties2csharp/Configuration.cs
@@ -4,5 +4,9 @@
     {
         public string ObjectType { get; set; } = "string";
         public string NumberType { get; set; } = "int";
+        public string PointNumberType { get; set; } = "double";
+        public string FloatType { get; set; } = "float";
+        public string BoolType { get; set; } = "bool";
+        public string DateType { get; set; } = "DateTime";
     }
 }
