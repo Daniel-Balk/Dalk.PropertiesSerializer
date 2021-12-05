55a89f3432e1a9c7f7e1308345a949b74b0c7352
Dalk-GitHub <67603460+Dalk-GitHub@users.noreply.github.com>
2021-12-05 18:09:43 +0100

    added electron check

---
+++ properties2csharp-website/wwwroot/app.js
@@ -0,0 +1,37 @@
+﻿console.log("loaded app.js")
+
+function isElectronX() {
+    // Renderer process
+    if (typeof window !== 'undefined' && typeof window.process === 'object' && window.process.type === 'renderer') {
+        return true;
+    }
+
+    // Main process
+    if (typeof process !== 'undefined' && typeof process.versions === 'object' && !!process.versions.electron) {
+        return true;
+    }
+
+    // Detect the user agent when the `nodeIntegration` option is set to true
+    if (typeof navigator === 'object' && typeof navigator.userAgent === 'string' && navigator.userAgent.indexOf('Electron') >= 0) {
+        return true;
+    }
+
+    return false;
+}
+
+function isElectron() {
+    var b = isElectronX();
+    console.warn(b);
+    if (b) {
+        return "true";
+    }
+    else {
+        return "false";
+    }
+}
+
+window.checkDownload = () => {
+    if (isElectron()) {
+        document.getElementById("blz").style.visibility = "hidden";
+    }
+}
